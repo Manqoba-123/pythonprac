fc13fdea5ba7f4121eca4347fd62c41f8e67d0fa
Lungile Nkabinde <lungile.nkabinde@takealot.com>
2023-03-30 23:52:10 +0200

    Perimeter and area of  a  rectangle

---
+++ first_week/exercise1_3.py
@@ -0,0 +1,13 @@
+#Calculating the perimeter and area  of  a rectangle
+
+
+length = float(input("Enter the length: "))
+width  =  float(input("Enter the width: "))
+
+
+perimeter = length  +  length + width + width
+area = length * width 
+
+print("perimeter = ", perimeter)
+print("area = ",  area)
+
