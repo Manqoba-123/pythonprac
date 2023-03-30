b315929fe72f045d8ebd793fb42369e78cc2301a
Manqoba-123 <mlangenimanqoba@gmail.com>
2023-03-30 23:37:56 +0200

    Updating files

---
+++ first_week/1-week_0.py
@@ -0,0 +1,9 @@
+r = float(input ("Enter your radius(r) :"))
+pi = 3.14159265359
+
+# a = input(Enter your radius) will take the input by default as a string
+
+area = pi * (r * r)
+
+print("r = ", r)
+print("Area =", area)
---
+++ first_week/check.py
@@ -0,0 +1,4 @@
+pi = 3.14
+a = 1.1
+answer = a * (pi ** 2)
+print(answer)
