17e9a4f1bfc7d5f5a3f7383f08e4a18e19ff329b
Manqoba-123 <mlangenimanqoba@gmail.com>
2023-03-30 23:59:33 +0200

    3-week

---
+++ first_week/3-week_0.py
@@ -0,0 +1,9 @@
+length = float(input ("Enter length: "))
+width = float(input ("Enter width: "))
+
+perimeter = ( 2 * length) + (2 * width)
+area = length * width
+
+print("perimeter =" + " " + str(perimeter))
+print("Area =" + " " + str(area))
+
