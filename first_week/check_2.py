233cfd18f4c93f52a9e2bdbd945e3f8b7b63fb03
Manqoba-123 <mlangenimanqoba@gmail.com>
2023-04-17 15:55:13 +0200

    Adding a file

---
+++ first_week/check_2.py
@@ -0,0 +1,5 @@
+first_name = input("Enter your name :")
+last_name = input("Enter your last name :")
+contact = int(input("Enter your contact number :"))
+
+print("Your first name :" + first_name + "\n" + "Your last name :" + last_name + "\n" + "And you contact "+ str(contact))
